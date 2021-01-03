bdf1266d5ab39a1bb5b57d3abbcd6471eb65fb52
inh2102 <61330943+inh2102@users.noreply.github.com>
2021-01-02 23:13:32 -0800

    function

---
+++ get_parler_news.R
@@ -0,0 +1,35 @@
+###########################################################
+############ SCRAPING NEWS ARTICLES ON PARLER #############
+###########################################################
+
+# Try this...
+system('timeout')
+
+# If 'timeout --help for more information.' is returned, PROCEED.
+
+# (If not, you need to install coreutils on your system.)
+system('brew install coreutils')
+# https://docs.brew.sh/Installation
+
+# Clone 'parlance' by castlelemongrab:
+system('git clone http://github.com/castlelemongrab/parlance')
+
+# Define the news-grabbing function:
+get_parler_news <- function(secs=100) {
+  system(paste0("cd parlance; timeout ",secs," parlance news > news.json"))
+  system('cd parlance; echo "\n]" >> news.json')
+}
+
+# Scrape news articles to ~/parlance/news.json
+get_parler_news()
+
+# Retrieve json data
+# install.packages('jsonlite')
+# install.packages('lubridate') - easy date manipulation
+
+# Tidy it up...
+news <- jsonlite::read_json("~/parlance/news.json",simplifyVector=TRUE)
+news_meta <- cbind(news$metadata['title'],news$metadata['description']) %>% tibble()
+news <- tibble(news) %>% select(createdAt,domain,long)
+news <- bind_cols(news,news_meta)
+news <- news %>% mutate(createdAt = lubridate::parse_date_time(createdAt,orders='ymd HMS'))
