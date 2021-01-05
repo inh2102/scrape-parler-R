150562c79d09312d5e43d9364c3171c998d494ed
inh2102 <61330943+inh2102@users.noreply.github.com>
2021-01-05 12:29:44 -0800

    Add files via upload

--- get_parler_news.R
+++ get_parler_news.R
@@ -14,21 +14,33 @@ system('brew install coreutils')
 # Clone 'parlance' by castlelemongrab:
 system('git clone http://github.com/castlelemongrab/parlance')
 
+# Credentials...
+do_credentials <- function(jst,mst) {
+  system(
+    paste0('cd parlance;','parlance init --mst ,',mst,
+           ' --jst ',jst,' -o config/auth.json'))
+}
+
+# Follow inh2102/scrape_parler Github instructions to find mst and jst...
+jst <- ''
+mst <- ''
+do_credentials(jst,mst)
+
 # Define the news-grabbing function:
-get_parler_news <- function(secs=100) {
+get_parler_news <- function(secs=10) {
   system(paste0("cd parlance; timeout ",secs," parlance news > news.json"))
   system('cd parlance; echo "\n]" >> news.json')
 }
 
 # Scrape news articles to ~/parlance/news.json
-get_parler_news()
+get_parler_news(secs=10)
 
 # Retrieve json data
 # install.packages('jsonlite')
-news <- jsonlite::read_json("~/parlance/news.json",simplifyVector=TRUE)
+# install.packages('lubridate') - easy date manipulation
 
 # Tidy it up...
-# install.packages('lubridate') - easy date manipulation
+news <- jsonlite::read_json("~/parlance/news.json",simplifyVector=TRUE)
 news_meta <- cbind(news$metadata['title'],news$metadata['description']) %>% tibble()
 news <- tibble(news) %>% select(createdAt,domain,long)
 news <- bind_cols(news,news_meta)
